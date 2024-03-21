d391b6d71a8fd3f40967bc770c290e1b8a01b40c
pharele2004 <159419668+pharele2004@users.noreply.github.com>
2024-03-21 10:14:37 +0100

    Update Controle.java

--- Programme/src/db/Controle.java
+++ Programme/src/db/Controle.java
@@ -1,5 +1,79 @@
 package db;
 
-public class Controle {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sound.midi.Track;
+
+public class Controleur2 {
+
+
+
+
+
+    public void addMedia(){
+
+
+    }
+
+    public void addComment(){
+
+    }
+
+    public void rechercher(String nom){
+
+
+    }
+
+    public void utilisateur(){
+
+    }
+
+    public void afficherBibliotheque() {
+
+    }
+
+    public void recommandation(){
+
+
+    }
+
+    public void createUser(){
+
+
+    }
+
+
+    public void avis(){
+
+    }
+
+    public void Rechercheravancée(){
+
+
+    }
+
+    public void description(){
+
+
+    }
+
+
+    // methode pour verifier si l'artiste est liker ou pas 
+    public void likeanddislike( boolean etat){
+      
+        if (etat = true){
+
+            etat = false;
+        }
+        else{
+            etat = true ;
+        }
+    }
+
+
+
+
 
 }
